8db379cf118335e86199402762ede1fc7961de38
LeonKim <suengyu@me.com>
2015-10-30 20:37:36 +0900

    first

---
+++ public/javascripts/main.js
@@ -0,0 +1,106 @@
+/**
+ * Created by LeonKim on 15. 10. 30..
+ */
+
+
+
+angular.module('sctDashModule', ['chart.js'])
+    .config(['ChartJsProvider', function (ChartJsProvider) {
+        // Configure all charts
+        ChartJsProvider.setOptions({
+            colours: ['#3F8CFF', '#FD9943', '#FD4343']
+        });
+        // Configure all line charts
+        ChartJsProvider.setOptions('Line', {
+            datasetFill: false
+        });
+    }])
+    .controller('mainCtrl', function ($scope) {
+
+    $scope.park_info = parkList[0];
+
+    gridster = $(".gridster ul").gridster({
+        widget_base_dimensions: [100, 100],
+        widget_margins: [5, 5],
+        autogrow_cols: true,
+        max_size_x : 20
+    }).data('gridster');
+
+    $scope.status = '오늘 운영시간 '+$scope.park_info.time_info[0];
+    $scope.abcdf = 'adfadfadsf';
+    $scope.isS0 = 'Y';
+    $scope.test = function(i,act){
+        if(act=='in'){
+            $('li:eq('+i+') .box_cnt').fadeIn();
+            $scope.isS0 = 'Y';
+        }else{
+            $('li:eq('+i+') .box_cnt').fadeOut();
+            $scope.isS0 = 'N';
+        }
+    };
+    $scope.labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug","Sep","Oct","Dec"];
+    $scope.series = ['H', 'M', 'L'];
+    $scope.option = {
+
+    };
+    $scope.data = [
+        [200, 190, 210, 208, 195, 178, 185,177,175,168,155,90],
+        [155, 167, 166, 176, 173, 158, 155,153,146,136,122,85],
+        [127, 125, 133, 131, 126, 122, 124,114,105,101,92,71]
+    ];
+
+    $scope.w = $('.maps').width();
+    $scope.site_info = site_info;
+    setTimeout(function() {
+        site_info.m.forEach(function (v, i, a) {
+            var targetm = '.td:eq(' + v + ')';
+            $(targetm).css('visibility', 'hidden');
+
+            var targets = '.minitd:eq(' + v + ')';
+            $(targets).css('visibility', 'hidden');
+        });
+
+        site_info.a.forEach(function (v, i, a) {
+            for (var t = 0; t < site_info.s.length; t++) {
+                if (site_info.s[t] == i) {
+
+                    var targetm = '.td:eq(' + v + ') div';
+                    $(targetm).css('background-color', '#51b9ff');
+
+                    var targets = '.minitd:eq(' + v + ') div';
+                    $(targets).css('background-color', '#51b9ff');
+                }
+            }
+        });
+
+        $('#siteView').css('visibility', 'visible');
+
+        $('#sidebar').height($('#sidebar').parent().height());
+    },200);
+
+
+});
+
+
+function crtlWnH() {
+    //각 컨텐츠 박스 Contents 높이
+    //common.js에 지정하면 .boxBs값을 가져오지 못 한 상태에서 각 위젯 컨텐츠 박스를 생성하여, 정상적인 높이로 만들어지지 않음
+    //본 함수때문인지는 몰라도 초기에 뿌려진 위젯 컨텐츠 박스 높이 이하로 줄여지지 않음
+    //UX플랫폼 개발진에 문의할 것
+    $(".box_cnt").each(function(){
+
+        $(this).css({'height':( $(this).parents(".boxBs").height() - 42 ) + 'px'});
+        //$(this).css({'height': '200px'});
+    });
+}
+
+
+var testbtn = function(){
+    $('#parkInfoModal').modal('show');
+};
+
+var tabClk = function(id){
+    console.log(id);
+    $('.tab-pane').hide();
+    $('#'+id).show();
+};
---
+++ public/javascripts/model.js
@@ -0,0 +1,194 @@
+/**
+ * Created by LeonKim on 15. 10. 30..
+ */
+var site_info = {x:[],y:[]},carTalk = {},myMk,myPoint,map, targetPoint;
+var rs = [];
+var int = null;
+var reserveDatas = [],rsData={};
+
+site_info.m = [
+    24,25,26,27,
+    28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
+    56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
+    84, 85, 86, 87, 109, 110, 111,
+    112, 113, 114, 115
+];
+
+site_info.a = [
+    117, 118, 119, 121, 122, 123, 125, 126, 127, 129,
+    130, 131, 133, 134, 135, 137, 138, 139
+];
+
+site_info.s = [
+    1,2,4,6,7,8,9,11,12,13,17
+];
+
+//init y information
+for(var y=0;y<5;y++){
+    if(y==1||y==2){
+        site_info.y.push({
+            y : y,
+            v : 'hidden'
+        });
+    }else{
+        site_info.y.push({
+            y : y,
+            v : null
+        });
+    }
+}
+
+//init x information
+for(var x = 0;x<28;x++){
+    if(x%4==0){
+        site_info.x.push({
+            x : x,
+            cm : 'Y',
+            c : 'black'
+        });
+    }else{
+        site_info.x.push({
+            x : x,
+            c : '#9b9b9b'
+        });
+    }
+}
+
+
+//Tue Oct 20 2015 10:32:01 GMT+0900 (KST)
+//Tue Oct 20 2015 10:38:01 GMT+0900 (KST)
+
+var parkList = [
+    {
+        park_id : 'test',
+        title : '옥산지하주차장',
+        loc : {
+            loc1 : '경상북도 경산시 서부1동 725-1',
+            loc2 : ''
+        },
+        geo_loc : {
+            lon : 128.726425,
+            lat : 35.8294583
+        },
+        tel : '(000)000-0000',
+        price : {
+            first : '-',
+            basic : {
+                t : 'M',
+                b : 10,
+                p : 500
+            },
+            max : 20000,
+            daily : '-'
+        },
+        time_info:[
+            '09:00 ~ 22:00','09:00 ~ 22:00','09:00 ~ 22:00'
+        ],
+        site : {
+            is_full : 'N',
+            now : 82,
+            total : 320
+        },
+        img : [
+            'img/sample/test_site_img.png',
+            'img/sample/test_site_img.png',
+            'img/sample/test_site_img.png'
+        ],
+        update_info : '11:37'
+    }
+];
+
+
+
+var userInfo = {
+    car_num : '23가1234',
+    tel : '010-0000-0000'
+};
+
+var find_result = [{
+    car_num : '23가1234',
+    locate : '3층 A구역 20',
+    pic : 'img/sample/sample_site.png',
+    init_time : "2015-10-13T05:15:18.526Z"
+},{
+    car_num : '17가2345',
+    locate : '3층 A구역 21',
+    pic : 'img/sample/sample_site.png',
+    init_time : "2015-10-13T03:39:18.526Z"
+}];
+
+var testRD = {
+    "user": {
+        "tel":"123",
+        "car_num":"123"
+    },
+    "reserve": {
+        "date":"2015-10-16T03:26:30.000Z",
+        "is_daily":"N",
+        "during":7200
+    }
+};
+
+//
+//var park_info = {
+//  park_id: '1211000001',
+//  title: '옥산지하주차장',
+//  address: {
+//    prev: '경북 경산시 옥산동 112번지',
+//    new: '경북 경산시 옥산동길 2가 212'
+//  },
+//  location: {
+//    latitude: 35.8294583,
+//    longitude: 128.726425
+//  },
+//  tel: '000-000-0000',
+//  price: {
+//    info : '유료',
+//    first: 0,		//최초주차금액 있을 시
+//    basic: {
+//      t: 'M',		//M : minute, H : hour, d : day
+//      b: 10,
+//      p: 500		// format : won
+//    },
+//    max: 20000, 	// 일단위 최고금액
+//    daily: 0		// 일주차요금
+//  },
+//  time_info: {
+//    weekday : {
+//      start : '09:00',
+//      end : '22:00'
+//    },
+//    weekend : {
+//      start : '09:00',
+//      end : '22:00'
+//    },
+//    holiday : {
+//      start : '09:00',
+//      end : '22:00'
+//    }
+//
+//  },
+//  site: {
+//    is_full: 'N',		// Y : 만차, N : 주차가능
+//    now: 82,		// 현재 주차가능 공간
+//    total: 320		// 총 주차공간
+//  },
+//  img: [		// 주차장 이미지 정보
+//    'file'			// type : File (jpg,png,gif)
+//  ],
+//  update_info: '2015-10-13T05:15:18.526Z'	// 정보전송 시간
+//};
+//
+//var site_info = [
+//  {
+//    park_id: '01100aok151020',	// 주차장 id
+//    site_code: '01',			// 주차공간 id
+//    site_name: 'A-1',			// 주차공간 이름
+//    car_info: {			// default : NULL, 차량 진출입에 의한 전달 시 정보 포함
+//      car_num: '12가1234',	// 차량 정보
+//      action: 'IN'			// IN : 진입, OUT : 진출
+//    },
+//    status: 'N',			// Y : 주차가능, N : 주차불가
+//    udt_time: '2015-10-13T05:15:18.526Z'
+//  }
+//];
